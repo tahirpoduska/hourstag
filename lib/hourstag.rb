7d25e938227feca4ccb33388bf0c1bc2bb466cdb
Tahir Poduska <tap87@cornell.edu>
2016-01-27 14:16:26 -0500

    New way to wire external middleware to steam

--- lib/hourstag.rb
+++ lib/hourstag.rb
@@ -1,12 +1,6 @@
-
-require 'locomotive/steam'
-require 'locomotive/steam/server'
 require 'rack/google-analytics'
+require 'locomotive/steam'
 
-  Locomotive::Steam.configure do |config|
-    path                = '.'
-    # config.mode         = :test
-    # config.adapter      = { name: :filesystem, path: File.expand_path(path) }
-    config.asset_path   = File.expand_path(File.join(path, 'public'))
-    config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-xxxxxxxx-1'
-  end
+Locomotive::Steam.configure_extension do |config|
+  config.middleware.insert_after Locomotive::Steam::Middlewares::Page, Rack::GoogleAnalytics, tracker: 'UA-xxxxxxxx-1'
+end
